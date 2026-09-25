ab30580d58711f3ff134d625e91053c1c828a05b
claireir0527-dev <claire.i.r0527@gmail.com>
2026-09-25 19:48:44 +0900

    Delete ClaireLifeCX directory

--- ClaireLifeCX/js/auth-guard.js
+++
@@ -1,1 +0,0 @@
-import {auth} from "./firebase.js";import{onAuthStateChanged}from"https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";onAuthStateChanged(auth,u=>{if(!u)location.replace("login.html")});
--- ClaireLifeCX/js/logout.js
+++
@@ -1,1 +0,0 @@
-import{auth}from"./firebase.js";import{signOut}from"https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";window.logoutClaireLife=async()=>{await signOut(auth);location.replace("login.html")};
